61ceab4773e13780a2ff9e9358d9c2b4e344bdfb
LHYWOOD <58466073+LHYWOOD@users.noreply.github.com>
2022-06-24 17:50:29 +0800

    Add files via upload

--- action.php
+++ action.php
@@ -1,55 +1,92 @@
-<?php
-	//session_start();
-	require_once 'vendor/autoload.php';	
-	use GuzzleHttp\Client;    
-	use GuzzleHttp\Psr7;
-	use GuzzleHttp\Exception\BadResponseException;
-	use GuzzleHttp\Psr7\Request;
-	use GuzzleHttp\Promise;
-    $clientid = "sunnytest";
-    //collect inputs
-	$termIp = $_POST['termIp'];
-    $merchantRef = $_POST['Merchant_Ref'];
-    $amt = (float) $_POST['amt'];
-    $type = $_POST['type'];
-	$client = new Client();
-	$headers = [
-	'Content-MD5' => '',
-	'Content-Type' => 'text/plain'
-	];
+<html>
+	<head>		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/md5.js"></script>
+	</head>
+	<body>
+		<?php
+			$termIp = $_POST['termIp'];
+			$type = $_POST['type'];								
+			$cmd = $_POST['cmd'];
+			switch ($type){
+				case "UNIONPAY":
+				case "VM":
+					$TYPE = "EDC";
+					break;
+				case "PAYME":
+				case "WECHAT":
+				case "ALIPAY":
+				case "ALIPAYHK":
+				case "FPS":
+					$TYPE = "SHOWQR";
+					break;
+				case "OCTOPUS":
+					$TYPE = "OCTOPUS";
+					break;
+			}
 
-	switch ($type){
-		case "UNIONPAY":
-		case "VM":
-			$TYPE = "EDC";
-			break;
-		case "PAYME":
-		case "WECHAT":
-		case "ALIPAY":
-		case "ALIPAYHK":
-		case "FPS":
-			$TYPE = "SHOWQR";
-			break;
-		case "OCTOPUS":
-			$TYPE = "OCTOPUS";
-			break;
-	}
-	$body = '{"TYPE":"'.$TYPE.'","CMD":"SALE","AMT":"'.$amt.'","ECRREF":"'.$merchantRef.'"}';
-	$request = new Request('POST', $termIp, $headers, $body);
-	$res = $client->sendAsync($request)->wait();
-	// echo "<p>" . $res->getBody() . "</p>";
-	$json1 = json_decode($body);
-	echo "<h2>Request array:</h2>";
-	foreach($json1 as $key1 => $val1){
-		echo $key1 . ": " . $val1;
-		echo "<br>";
-	}	
-	echo "<br><br><hr>";
-	$json = json_decode($res->getBody());
-	echo "<h2>Respond array:</h2>";
-	foreach($json as $key => $val){
-		echo $key . ": " . $val;
-		echo "<br>";
-	}	
-		
-?>
+			switch ($cmd){
+				case "SALE":
+					$merchantRef = $_POST['Merchant_Ref'];					 
+					$amt = (float) $_POST['amt'];
+					$body = '{\"TYPE\":"'.$TYPE.'",\"CMD\":\"SALE\",\"AMT\":"'.$amt.'",\"ECRREF\":"'.$merchantRef.'"}';
+					$payload = '{"TYPE":"'.$TYPE.'","CMD":"SALE","AMT":"'.$amt.'","ECRREF":"'.$merchantRef.'"}';	
+					break;					
+				case "VOID":					 
+					$trace = $_POST['trace'];	
+					$body = '{\"TYPE\":"'.$TYPE.'",\"CMD\":\"VOID\",\"TRACE\":"'.$trace.'"}';
+					$payload = '{"TYPE":"EDC","CMD":"VOID","TRACE":"'.$trace.'"}';	
+					break;
+				case "REFUND":
+					$amt = (float) $_POST['amt'];
+					$refnum = $_POST['refnum'];
+					$body = '{\"AMT\":"'.$amt.'",\"REFNUM\":"'.$refnum.'",\"CMD\":\"REFUND\",\"TYPE\":"'.$TYPE.'"}';
+					$payload = '{"AMT":1010.00,"REFNUM":"123456789012","CMD":"REFUND","TYPE":"EDC"}';
+					break;
+				case "INSTALMENT":
+					$merchantRef = $_POST['Merchant_Ref'];
+					$amt = (float) $_POST['amt'];
+					$tender = (int) $_POST['tender'];
+					$body = '{\"TENDER\":"'.$tender.'",\"AMT\":"'.$amt.'",\"ECRREF\":"'.$merchantRef.'",\"CMD\":\"INSTALMENT\",\"TYPE\":"'.$TYPE.'"}';
+					$payload = '{"TENDER":"'.$tender.'","AMT":"'.$amt.'","ECRREF":"'.$merchantRef.'","CMD":"INSTALMENT","TYPE":"'.$TYPE.'"}';
+			}	
+			$hash = md5($payload);
+			echo "<center><h1>Waiting...</h1></center>";
+		?>
+		<script script type="text/javascript">
+			//function request(){
+			const data = '<?=$body?>';
+			var settings = {
+			"url": "http://<?=$termIp?>",
+			"method": "POST",
+			"timeout": 0,
+			"headers": {
+				//"Content-MD5": "",
+				"Content-Type": "text/plain"
+			},
+			"data": data,
+			};
+							
+			$.ajax(settings).done(function (response) {
+				var payload = '<?=$payload?>';
+				var hash = '<?=$hash?>';
+				var payload = jQuery.parseJSON(payload);
+				document.write("<h2>Request array: </h2>");
+				$.each(payload,function(key, value){
+					document.write(key + ': ' + value + '<br>');
+				});
+				document.write("<p>Hash: "+hash+"</p>");
+				var json = response;
+				var result = jQuery.parseJSON(json);		
+				document.write("<br><hr><h2>Respond array: </h2>");
+				$.each(result,function(key, value){
+					console.log(key + ': ' + value);
+					document.write(key + ': ' + value + '<br>');			
+				});
+			});
+			//}
+
+        </script>		
+	</body>
+</html>
